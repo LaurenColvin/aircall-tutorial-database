375ca837a61c756b5391fbf234ed7c6a1c9182f7
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-03-01 11:18:51 -0800

    add /vinyls route

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const vinylController = require ('./controllers/vinylController.js')
 
 const PORT = process.env.PORT
 
 const app = express()
 app.use(logger('dev'));
 app.use(cors())
+
+app.use('/vinyls', vinylController)
 
 app.get('/', function (req, res) {
     res.send('hello earth!')
