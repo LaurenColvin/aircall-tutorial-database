d8f52bd6014f3c0b20c8568fd6bd13f412aefaf6
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-03-02 15:39:13 -0800

    adding conditional for heroku db

--- connection.js
+++ connection.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose')
+let mongoUrl = ""
+if (process.env.NODE_ENV==="production") {
+    mongoUrl = process.env.DB_URL;
+} else {
+    mongoUrl = "mongodb://127.0.0.1/vinyl-api"
+}
 
-mongoose.connect("mongodb://127.0.0.1/vinyl-api")
+mongoose.connect(mongoUrl)
 // mongoose.connect("mongodb://localhost/vinyl-api")
 
 module.exports = mongoose
